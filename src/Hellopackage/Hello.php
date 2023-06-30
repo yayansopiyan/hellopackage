f6a54525d4ae637656acedf205af88127994a4ca
yayan sopiyan <yayan.sopiyan@gmail.com>
2023-06-30 10:43:29 +0700

    Update .gitignore, .php-cs-fixer.php, and 3 more files...

--- src/Hellopackage/Hello.php
+++ src/Hellopackage/Hello.php
@@ -1,12 +1,18 @@
 <?php
 
+declare(strict_types=1);
+/**
+ * This file is part of Atkalab.
+ *
+ * @author   Yayan Sopiyan,
+ * @contact  yayan.sopiyan@gmail.com
+ */
 namespace Atkalab\Hellopackage;
 
 class Hello
 {
     public function sayHello()
     {
-        echo "Hello";
+        echo 'Hello';
     }
-
 }
